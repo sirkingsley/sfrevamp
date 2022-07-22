5d569fa1db628a6b76fa606127ceb88815fd8fe0
Silas Sangmin <sangminsilas@gmail.com>
2022-07-22 19:54:01 +0000

    Lact -mat worked on

--- src/app/components/commons/sign-up/sign-up.component.ts
+++ src/app/components/commons/sign-up/sign-up.component.ts
@@ -38,20 +38,20 @@ export class SignUpComponent implements OnInit {
     },);
   }
 
-  MustMatch(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-    const control = formGroup.controls[controlName];
-    const matchingControl = formGroup.controls[matchingControlName];
-    if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
-    return;
-    }
-    if (control.value !== matchingControl.value) {
-    matchingControl.setErrors({ mustMatch: true });
-    } else {
-    matchingControl.setErrors(null);
-    }
-    }
-    }
+  // MustMatch(controlName: string, matchingControlName: string) {
+  //   return (formGroup: FormGroup) => {
+  //   const control = formGroup.controls[controlName];
+  //   const matchingControl = formGroup.controls[matchingControlName];
+  //   if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+  //   return;
+  //   }
+  //   if (control.value !== matchingControl.value) {
+  //   matchingControl.setErrors({ mustMatch: true });
+  //   } else {
+  //   matchingControl.setErrors(null);
+  //   }
+  //   }
+  //   }
 
   onCountryChanged(country) {
     console.log(country);
